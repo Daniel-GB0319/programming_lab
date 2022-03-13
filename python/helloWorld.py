2f4b88271ccf1467a0776f99d6f84ba9b841fad9
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2022-03-13 02:17:17 -0600

    Agregada carpeta Compiladores en python. Actualizado helloWorld.py para ficheros.

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,3 +1,24 @@
+import os
+import random
+
+numero = 0
+file = open("cadenas.txt","r")
+cadenas = file.read().splitlines()
+file.close()
+
+# ---- Programa Principal ----
+os.system("cls")
+print(f"Aqui esta el contenido de cadenas.txt ")
+print(cadenas)
+print(type(cadenas))
+
+# Genera numeros binarios para despues guardarlos en fichero
+file = open("cadenas.txt","w")
+for i in range(10):
+    numero = random.randint(512,1024) # Genera numeros enteros aleatorios entre 512 y 1024
+    file.write(f"{bin(numero)[2:]}\n") # Escribe los numeros generados en binario dentro del fichero
+file.close()
+
 # Esto 
 # es 
 # un 
