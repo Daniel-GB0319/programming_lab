867d60f109b5a697923d756b5cc46010fe85d0d5
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2022-03-22 16:55:37 -0600

    Finalizado el examen.py en A. Algoritmos

--- python/Analisis_Algoritmos/examen1/examen1.py
+++ python/Analisis_Algoritmos/examen1/examen1.py
@@ -5,27 +5,32 @@ import os
 # *************************** Funciones ************************************
 # **************************************************************************
 
-# Esta funcion se encarga de llenar la tabla de amortizacion
-def tabAmortizacion (renta,saldo,tasaPeriodo,i):
+# Esta funcion se encarga de llenar la tabla de amortizacion recursivamente
+def tabAmortizacion (renta,saldo,tasaPeriodo,i,total):
 	interes=0
 	amortizacion=0
 	
 	# Caso Base
-	if saldo <= 0:
+	if saldo <= 0: # Se verifica si el saldo ha llegado a cero
 		return 0
 	
+	# Caso recursivo
 	else:
-		i = i + 1
-		interes = round(saldo*tasaPeriodo, 2)
-		amortizacion = round(renta - interes, 2)
-		saldo = round(saldo-amortizacion, 2)
+		i = i + 1 # Numero de Pago se incrementa
+		interes = round(saldo*tasaPeriodo, 2) # Se calcula el interes
+		amortizacion = round(renta - interes, 2) # Se calcula la amortizacion
+		saldo = round(saldo-amortizacion, 1) # Se calcula el nuevo saldo 
 		
-		tabla.append([i,renta,interes,amortizacion,saldo])  
-		
-		tabAmortizacion(renta,saldo,tasaPeriodo,i)
-		
-	return 0
+		# Se suman los valores obtenidos al total
+		total[1] = total[1] + renta
+		total[2] = total[2] + interes
+		total[3] = total[3] + amortizacion
 
+		tabla.append([i,renta,interes,amortizacion,saldo]) # Se ingresa la fila con los datos calculados a la tabla de amortizacion 
+		
+		tabAmortizacion(renta,saldo,tasaPeriodo,i,total) # Se vuelve a llamar a la funcion para trabajar con nuevo saldo
+
+	return total
 
 
 # ***********************************************************************************
@@ -37,26 +42,34 @@ saldo = 0 # Guarda el Capital Prestado
 numPagos = 0 # Guarda el numero de años que dura el pago
 tasaPeriodo = 0 # Guarda la tasa de interes por periodo de capitalizacion
 i = 0 # Guarda el numero de pago
-renta = 0 
+renta = 0 # Guarda el valor calculado de la renta fija
+total = ["TOTAL",0,0,0,"-"] # Guarda el total de la renta, interes y amortizacion calculados
 
-# --------- Instrucciones
-os.system("clear") # Limpia pantalla 
+# --------- Instrucciones -----------
+os.system("cls") # Limpia pantalla 
 print("\n******* Gonzalez Barrientos Geovanni Daniel 3CV14  Analisis de Algoritmos *******\n")
 print("------- Examen 1 - Tabla de Amortizacion -------\n\n")
 print("INSTRUCCIONES: Este programa calculara los valores de amortizacion segun el capital, plazo y tasa ingresados\n\n")
 
-saldo = (input("\nIngrese el Saldo Inicial: "))
+# Se ingresan los valores necesarios para el calculo
+saldo = int(input("\nIngrese el Saldo Inicial: "))
 numPagos = int(input("\nIngrese el Plazo en años: "))
 tasaPeriodo = int(input("\nIngrese la Tasa por Periodo de Capitalizacion (%): "))
 tasaPeriodo = tasaPeriodo / 100
-print(tasaPeriodo)
 
-renta=round( ( saldo*tasaPeriodo)/(1-((1+tasaPeriodo)**(-numPagos))), 2 )
-print(renta)
+# Se calcula la renta fija
+renta = round( ( saldo*tasaPeriodo)/(1-((1+tasaPeriodo)**(-numPagos))), 2 )
 
+# Se inicializa la tabla de amortizacion
 tabla = [["Periodo","Renta","Interes","Amortizacion","Saldo"],[i,"-","-","-",saldo]]
 
-tabAmortizacion (renta,saldo,tasaPeriodo,i)
+# Se hace el llamado a la funcion que se ejecutara recursivamente
+total = tabAmortizacion (renta,saldo,tasaPeriodo,i,total)
+
+# Se ingresan los montos totales a la tabla
+tabla.append(total)
+
+# Se imprime la tabla
 print(tabulate(tabla, headers='firstrow', tablefmt='psql'))
 
 print("\n!!! Gracias por utilizar el programa !!! Hasta luego ;)\n")
