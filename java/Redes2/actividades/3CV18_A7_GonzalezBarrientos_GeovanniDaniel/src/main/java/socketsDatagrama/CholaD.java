c599e44c95c0ec7a028a1116cac01355d97aee4d
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2022-04-25 03:40:35 -0500

    Modificado actividad7 en Redes2. Falta agregar funcionalidad para enviar y recibir varios paquetes cuando el mensaje excede la longitud maxima del datagrama.

--- java/Redes2/actividades/3CV18_A7_GonzalezBarrientos_GeovanniDaniel/src/main/java/socketsDatagrama/CholaD.java
+++ java/Redes2/actividades/3CV18_A7_GonzalezBarrientos_GeovanniDaniel/src/main/java/socketsDatagrama/CholaD.java
@@ -6,17 +6,32 @@ public class CholaD {
     
     public static void main(String[] args){
         try{
-            try (DatagramSocket cl = new DatagramSocket()) { // Se inicializa socket para cliente
-                System.out.print("***** Cliente Iniciado ***** \nA continuacion escriba un mensaje: ");
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // Se inicializa buffer para leer texto del usuario
-                String mensaje = br.readLine(); // Se realiza la lectura de texto desde el teclado
-                byte[] b = mensaje.getBytes(); // Se realiza la conversion del mensaje en bytes para su posterior envio al servidor
-                String dst ="127.0.0.1"; // Se indica la direccion del servidor
-                int pto = 2000; // Se indica el puerto del servidor
-                DatagramPacket p = new DatagramPacket(b,b.length,InetAddress.getByName(dst),pto); // Se crea el paquete a enviar
-                cl.send(p); // Se envia el paquete al servidor
-                cl.close(); // Se cierra el socket del cliente 
-            }
+            DatagramSocket cl = new DatagramSocket();  // Se inicializa socket para cliente
+            System.out.print("***** CLIENTE INICIADO ***** \nA continuacion escriba un mensaje: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // Se inicializa buffer para leer texto del usuario
+
+            String mensaje = br.readLine(); // Se realiza la lectura de texto desde el teclado
+
+            byte[] b = mensaje.getBytes(); // Convierte el mensaje en bytes para su posterior envio al servidor
+
+            InetAddress dst = InetAddress.getByName("localhost"); // Se indica la direccion del servidor
+            int pto = 2000; // Se indica el puerto del servidor
+
+            DatagramPacket p = new DatagramPacket(b,b.length,dst,pto); // Se crea el paquete a enviar
+            
+            System.out.println("!!! Datagrama enviado a " + dst + " : " + pto + " !!!\n");
+            
+            cl.send(p); // Se envia el paquete al servidor
+            
+            System.out.print("A continuacion el servidor reenviara el mensaje para verificar su integridad.\n");
+            
+            cl.receive(p);// Se recibe el paquete enviado desde el servidor para funcionalidad eco
+            
+            String msjServer = new String(p.getData(),0,p.getLength()); // Se guarda el mensaje recibido en variable para imprimir en pantalla
+            System.out.println("Mensaje recibido desde el servidor : " + msjServer + "\n");
+            cl.close(); // Se cierra el socket del cliente 
+
+            System.out.print("***** CONEXION FINALIZADA *****");
         }catch(IOException e){
         }// catch
     }// main
--- java/Redes2/actividades/3CV18_A7_GonzalezBarrientos_GeovanniDaniel/src/main/java/socketsDatagrama/SholaD.java
+++ java/Redes2/actividades/3CV18_A7_GonzalezBarrientos_GeovanniDaniel/src/main/java/socketsDatagrama/SholaD.java
@@ -6,17 +6,39 @@ public class SholaD {
 
     public static void main(String[] args) {
         try{
-            try (DatagramSocket s = new DatagramSocket(2000)) { // Se inicializa el socket para el servidor
-                System.out.println("***** Servidor Iniciado ***** \nEsperando a un cliente... \n");
+            DatagramSocket s = new DatagramSocket(2000);  // Se inicializa el socket para el servidor con el puerto 2000
+            System.out.println("***** SERVIDOR INICIADO ***** \nEsperando paquete de un cliente... \n");
+
+            for(;;){ // Bucle para atender a los clientes
+                byte[] b = new byte[2000]; 
+                int ptoCliente;
+                InetAddress dirCliente;
+
+                DatagramPacket p = new DatagramPacket(b,b.length);
+
+                s.receive(p);// Se recibe el paquete desde el cliente
+
+                ptoCliente = p.getPort(); // Se obtine el puerto del cliente
+                dirCliente = p.getAddress(); // Se obtine la direccion del cliente
+
+                System.out.println("!!! Datagrama recibido desde " + dirCliente + " : " + ptoCliente + " !!!\n");
+                String msj = new String(p.getData(),0,p.getLength()); // Se guarda el mensaje en variable para imprimir en pantalla
+                System.out.println("Mensaje recibido del cliente: " + msj + "\n");
                 
-                for(;;){ // Bucle para atender a los clientes
-                    DatagramPacket p = new DatagramPacket(new byte[2000],2000);
-                    s.receive(p);// Se recibe el paquete desde el cliente
-                    System.out.println("Datagrama recibido desde " + p.getAddress() + " : " + p.getPort() + "\n");
-                    String msj = new String(p.getData(),0,p.getLength()); // Se guarda el mensaje en variable para imprimir en pantalla
-                    System.out.println("Mensaje recibido : " + msj);
-                }// for
-            }
+                System.out.println("A continuacion se reenviara el mensaje recibido al cliente para verificar su integridad.\n");
+                
+                b = msj.getBytes(); // Se convierte el mensaje recibido en bytes
+                
+                p.setAddress(dirCliente); // Se asigna la direccion del cliente al paquete
+                p.setPort(ptoCliente); // Se asigna el puerto del cliente al paquete
+                p.setData(b); // Se asigna el mensaje del cliente al paquete
+                p.setLength(b.length); // Se asigna la longitud del mensaje del cliente al paquete
+                
+                s.send(p); // Se reenvia el paquete recibido al cliente
+                System.out.println("!!! Mensaje reenviado al cliente. !!!\n");
+                System.out.println("***** CONEXION CON CLIENTE FINALIZADO ***** \n\nEsperando paquete de otro cliente... \n");
+            }// for
+
         }catch(IOException e){
         }// catch
     }// main
