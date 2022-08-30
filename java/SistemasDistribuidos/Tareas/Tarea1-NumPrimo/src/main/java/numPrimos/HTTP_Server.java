03e0b6555b90ec228d1c002efbea8239ee4a3a89
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2022-08-30 02:44:57 -0500

    Finalizada Tarea1 en SistemasDistribuidos. Se reestructuro el codigo para la creacion de un hilo para el navegador web y 4 subhilos para el Servidor A.

--- java/SistemasDistribuidos/Tareas/Tarea1-NumPrimo/src/main/java/numPrimos/HTTP_Server.java
+++ java/SistemasDistribuidos/Tareas/Tarea1-NumPrimo/src/main/java/numPrimos/HTTP_Server.java
@@ -2,34 +2,81 @@ package numPrimos;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HTTP_Server {
     static class Servidor extends Thread{ // Clase para Hilos
         Socket conexionWeb = null; // Socket para el cliente en el navegador web
         int puerto; // Puerto para conectarse a un Servidor A
-        static String primo; // Respuesta que se enviara a navegador web
+        String name; // Nombre del hilo en cuestion
+        static long numero; // Numero obtenido de URL
+        static String primo; // Verifica si numero de URL es primo o no
         static Object obj = new Object(); // Objeto para sincronizar los hilos
-        static int hilos; // Sirve para imprimir resultado final al finalizar todos los hilos
         
-        Servidor(Socket conexionWeb, int puerto){ // Constructor Servidor
+        Servidor(Socket conexionWeb, int puerto, String name){ // Constructor Servidor
             this.conexionWeb = conexionWeb; // Se asigna socket del cliente en navegador web
             this.puerto = puerto; // Puerto para conectarse a un Servidor A
+            this.name = name; // Nombre del hilo actual
         } // constructor Servidor
         
         // Procedimientos a realizar en los hilos
         public void run(){ 
             try{
-                BufferedReader entrada = new BufferedReader(new InputStreamReader(conexionWeb.getInputStream()));
-                PrintWriter salida = new PrintWriter(conexionWeb.getOutputStream());
-                String url = entrada.readLine(); // URL ingresada
-                
-                if(url.startsWith("GET /es_primo?n=")){ // Se verifica que URL es correcta
+                Thread.currentThread().setName(name);
+                if("Hilo-WEB".equals(Thread.currentThread().getName())){ // Procedimiento para hilo de navegador WEB
+                    BufferedReader entrada = new BufferedReader(new InputStreamReader(conexionWeb.getInputStream()));
+                    PrintWriter salida = new PrintWriter(conexionWeb.getOutputStream());
+                    String url = entrada.readLine(); // URL ingresada en navegador
+                    
+                    if(url.startsWith("GET /es_primo?")){ // Se verifica que URL es correcta  
+                        Servidor server[] = new Servidor[4]; // Sockets para servidores A
+                        int j=0;
+                        
+                        // Se obtiene numero de URL por medio de subcadena 
+                        String num_url = url.substring(url.indexOf("=")+1, url.lastIndexOf("H")-1); 
+                        synchronized(obj){ // Se guarda numero a trabajar por los hilos
+                            numero = Long.parseLong(num_url);
+                            System.out.println("Numero recibido desde URL: "+numero);
+                        } // synchronized
+                                               
+                        for(int i=4000; i<4004;i++){  //Se crean los hilos para los servidores A  
+                            server[j] = new Servidor(conexionWeb,i,"Hilo-SA"+j);
+                            j++;
+                        } // for
+
+                        for(int i=0; i<4;i++){ //Se inician los hilos para los servidores A
+                            server[i].start();
+                        } // for
+
+                        for(int i=0; i<4;i++){ //Se crea barrera de hilos
+                            server[i].join();
+                        } // for  
+
+                        // Mensajes del protocolo HTTP
+                        String resp_web = "<html><button onclick='alert(\""+numero+" "+primo+"\")'>Ver Resultado</button></html>";
+                        salida.println("HTTP/1.1 200 OK");
+                        salida.println("Content-type: text/html; charset=utf-8");
+                        salida.println("Content-lengh: "+resp_web.length());
+                        salida.println();
+                        salida.flush();
+                        salida.println("Server: Servidor_HTTP.java\n");
+                        salida.flush();
+                        salida.println("Date: "+ new Date()+"\n");
+                        salida.flush();
+                        salida.println(conexionWeb.getRemoteSocketAddress().toString()+"\n" );
+                        salida.flush();
+                        salida.println(resp_web);
+                        salida.flush();
+                    }else{ // URL es incorrecta
+                        salida.println("HTTP/1.1 404 File Not Found");
+                        salida.flush();
+                    } // else
+                    
+                }else{ // Procedimiento para hilos que se conectaran a SERVER A
                     Socket cl = new Socket("localhost", puerto); // Se crea conexion con un Servidor A
-                    System.out.println("SOCKET CREADO EN SERVER-A CON (PUERTO :"+cl.getPort()+" // HILO: "+ Thread.currentThread().getName());
                     DataInputStream dis = new DataInputStream(cl.getInputStream() ); // Flujo entrada Servidor A
                     DataOutputStream dos = new DataOutputStream(cl.getOutputStream()); // Flujo salida Servidor A
-                    String resp_server_a = url.substring(url.indexOf("=")+1, url.lastIndexOf("H")-1); // Respuesta del Servidor A
-                    long numero = Long.parseLong(resp_server_a);
                     long num_inicial; // Limite inferior del rango en Servidor A
                     long num_final; // Limite SUPERIOR del rango en Servidor A
           
@@ -50,51 +97,27 @@ public class HTTP_Server {
                             num_inicial = ((numero/4)*3)+1;
                             num_final = numero-1;
                         }
-                    }// switch
+                    }// switch rangos para servidor a
                   
                     // Se envian el numero ingresado en URL y el rango al Servidor A
                     dos.writeLong(numero);
                     dos.writeLong(num_inicial);
                     dos.writeLong(num_final);
                     
-                    resp_server_a = dis.readUTF(); // Se recibe respuesta de Servidor A
-                    System.out.println("RESPUESTA RECIBIDA DE SERVER-A ("+cl.getInetAddress()+" // "+cl.getPort()+") : "+ resp_server_a);
+                    String resp_server_a = dis.readUTF(); // Se recibe respuesta de Servidor A
+                    // Se almacena resultado de cada servidor A
                     synchronized(obj){
                         if("NO DIVIDE".equals(resp_server_a) && !"NO ES PRIMO".equals(primo)){
                             primo = "ES PRIMO";
                         }else{
                             primo = "NO ES PRIMO";
                         } // ELSE
-                        System.out.println("Primo : "+primo);
-                        System.out.println("HILOS ENTRADA : "+hilos);
-                        if(hilos==3){ // Verifica si ya terminaron todos los hilos
-                            String resp_web = "<html><button onclick='alert(\""+primo+"\")'>Ver Resultado</button></html>";
-                            salida.println("HTTP/1.1 200 OK");
-                            salida.println("Content-type: text/html; charset=utf-8");
-                            salida.println("Content-lengh: "+resp_web.length());
-                            salida.println();
-                            salida.flush();
-                            salida.println("Server: Servidor_HTTP.java");
-                            salida.flush();
-                            salida.println("Date: "+ new Date());
-                            salida.flush();
-                            salida.println(conexionWeb.getRemoteSocketAddress().toString() );
-                            salida.flush();
-                            salida.println(resp_web);
-                            salida.flush();
-                        }else{
-                            hilos++;
-                            System.out.println("HILOS SALIDA : "+hilos);
-                        } // else
                     } // synchronized
-                    cl.close(); // Se cierra conexion con Servidor A
-                    System.out.println("SOCKET FINALIZADO EN SERVER-A CON (PUERTO :"+cl.getPort()+" // HILO: "+ Thread.currentThread().getName());
-                }else{
-                    salida.println("HTTP/1.1 404 File Not Found");
-                    salida.flush();
-                } // else
-
+                    cl.close(); // Se cierra conexion con Servidor A  
+                } //else procedimiento para Servidor A
             }catch(IOException | NumberFormatException e){
+            } catch (InterruptedException ex) {
+                Logger.getLogger(HTTP_Server.class.getName()).log(Level.SEVERE, null, ex);
             } // catch
         } // run
     } // clase Servidor 
@@ -102,33 +125,22 @@ public class HTTP_Server {
     // Funcion main
     public static void main(String[] args) throws Exception {
         ServerSocket s = new ServerSocket(8080); // Socket para servidor HTTP
-        Servidor server[] = new Servidor[4]; // Sockets para servidores A
-          int j=0;
-        
-        System.out.println("\n\n*** SERVIDOR HTTP INICIADO *** Puerto: " + s.getLocalPort() );
+        Servidor serverWeb = null ; // Se inicializa el objeto para thread para el navegador web
+       
+        System.out.println("\n\n%% Gonzalez Barrientos Geovanni Daniel - Tarea 1 - Sistemas Distribuidos 4CV13 %%");
+        System.out.println("\n*** SERVIDOR HTTP INICIADO *** Puerto: " + s.getLocalPort() );
         System.out.println("Esperando conexion de un cliente desde el navegador... ");
         for(;;){
-            Socket conexionWeb = s.accept();
-            System.out.println("-- Conexion con Cliente Iniciada -- IP: " + conexionWeb.getInetAddress());
-
-            j=0;
-            for(int i=4000; i<4004;i++){
-                server[j] = new Servidor(conexionWeb,i);
-                j++;
-            } // for
-
-            for(int i=0; i<4;i++){
-                server[i].start();
-            } // for
-
-            for(int i=0; i<4;i++){
-                server[i].join();
-                System.out.println("HILO #"+i+" ESPERANDO");
-            } // for  
-
-            conexionWeb.close();
+            try (Socket conexionWeb = s.accept()) {
+                System.out.println("\n-- Conexion con Cliente Iniciada -- IP: " + conexionWeb.getInetAddress());
+                serverWeb = new Servidor(conexionWeb,0,"Hilo-WEB");
+                
+                // Se inicia el hilo del navegador Web
+                serverWeb.start();
+                serverWeb.join();
+            } // try
             System.out.println("-- Conexion con Cliente Finalizada --");
-            System.out.println("Esperando conexion de otro cliente desde el navegador... ");
+            System.out.println("\nEsperando conexion de otro cliente desde el navegador... ");
         } // for
     } // main
 }
--- java/SistemasDistribuidos/Tareas/Tarea1-NumPrimo/src/main/java/numPrimos/Server_A.java
+++ java/SistemasDistribuidos/Tareas/Tarea1-NumPrimo/src/main/java/numPrimos/Server_A.java
@@ -14,26 +14,26 @@ public class Server_A {
         // Procedimiento a realizar en el hilo
         public void run(){
             try{
-                DataInputStream dis = new DataInputStream(cl.getInputStream() );
-                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
-                System.out.println("-- Conexion con Cliente Iniciada -- IP: " + cl.getInetAddress());
+                DataInputStream dis = new DataInputStream(cl.getInputStream()); // Flujo de entrada
+                DataOutputStream dos = new DataOutputStream(cl.getOutputStream()); // Flujo de salida
+                System.out.println("\n-- Conexion con Cliente Iniciada -- IP: " + cl.getInetAddress());
                 
                 String respuesta = "NO DIVIDE"; 
-                long n;
-                long numero = dis.readLong();
-                long numero_inicial = dis.readLong();
-                long numero_final = dis.readLong();
+                long n; 
+                long numero = dis.readLong(); // Numero de URL
+                long numero_inicial = dis.readLong(); // Limite inferior
+                long numero_final = dis.readLong(); // Limite Superior
                 
+                // Bucle para realizar todas las divisiones dentro del rango
                 for(n = numero_inicial; n < numero_final+1; n++){
-                    if(numero%n == 0){
+                    if(numero%n == 0){ // Verifica si hay una division exacta
                         respuesta = "DIVIDE";
                         break;
                     } // if
                 } // for
                 System.out.println("Respuesta final obtenida: " + respuesta);
                 
-                dos.writeUTF(respuesta);
-                //cl.close();
+                dos.writeUTF(respuesta); // Se envia respuesta a Servidor HTTP
                 System.out.println("-- Conexion con Cliente Finalizada --");
                 System.out.println("\nEsperando conexion de otro cliente... ");
             }catch(IOException e){
@@ -44,20 +44,24 @@ public class Server_A {
     
     public static void main(String[] args) throws Exception {
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        int puerto;
+        int puerto; // Almacena el puerto para abrir el Servidor
         
         do{
             System.out.println("\n\nPuerto para esta instancia del Servidor A (4000 - 4003): ");
-            puerto = Integer.parseInt(entrada.readLine());
+            puerto = Integer.parseInt(entrada.readLine()); // Lee puerto desde teclado
         }while(puerto<4000 && puerto>4003);
        
-        ServerSocket s = new ServerSocket(puerto);
-        System.out.println("*** SERVIDOR INICIADO *** Puerto: " + s.getLocalPort() );
+        ServerSocket s = new ServerSocket(puerto); // Se crea socket para Servidor
+        System.out.println("\n\n%% Gonzalez Barrientos Geovanni Daniel - Tarea 1 - Sistemas Distribuidos 4CV13 %%");
+        System.out.println("\n*** SERVIDOR INICIADO *** Puerto: " + s.getLocalPort() );
         System.out.println("Esperando conexion de un cliente...");
         for(;;){
-            Socket cl = s.accept();
-            Calculadora cal = new Calculadora(cl);
-            cal.start();
+            try (Socket cl = s.accept() // Se conecta un cliente
+            ) {
+                Calculadora cal = new Calculadora(cl); // Se crea una clase para hilo
+                cal.start(); // Se inicia el hilo
+                cal.join();
+            } // Se crea una clase para hilo
         } // for
     } // main()
 }
